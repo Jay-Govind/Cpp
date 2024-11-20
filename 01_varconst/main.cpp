315812ca710f36f5a9fb6ee3a21969552914fe6d
Jay Govind Kumar <govind.iq@gmail.com>
2024-11-20 11:50:26 +0530

    Add the files

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int score = 100;
+    score = 200;
+    cout << score << endl;
+
+    const int marks = 100;
+    // marks = 200;
+
+    cout << marks << endl;
+
+    return 0;
+}  
