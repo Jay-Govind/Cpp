d42de7c124e4f435a643bb088298e1158419a599
Jay Govind Kumar <govind.iq@gmail.com>
2024-11-20 12:20:00 +0530

    add the main file

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 10;
+    cout << number << endl;
+
+    float temperature = 25.5;
+    cout << temperature << endl;
+
+    double pi = 3.14159;
+    cout << pi << endl;
+
+    char letter = 'A';
+    letter = 'AB';          // This will store `B`
+    cout << letter << endl;
+
+    bool isTrue = 5;
+    cout << isTrue << endl;
+
+    return 0;
+}
