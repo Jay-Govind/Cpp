0613a5d41f562fb1b1aeb38713936e26a45e1be3
Jay Govind Kumar <govind.iq@gmail.com>
2024-11-20 13:25:49 +0530

    add the files 'modifier'

---
+++ 02_primitives/modifier.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 10;
+    cout << number << endl;
+
+    float number2 = 100000000;
+    cout << number2 << endl;
+
+    short number3 = 10;
+    cout << number3 << endl;
+
+    unsigned number4 = 10;
+    cout << number4 << endl;
+
+    long number5 = 10458820;
+    cout << number5 << endl;
+
+    long long number6 = 105405450065184751;
+    cout << number6 << endl;
+
+    return 0;
+}
