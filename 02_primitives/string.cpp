7254e47dc92d3b98699be659b9ab3a6243b8527a
Jay Govind Kumar <govind.iq@gmail.com>
2024-11-21 11:07:58 +0530

    add the files

---
+++ 02_primitives/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string name = "Jay Govind";
+    cout << name << endl;
+
+    string quote = "\"The man who does not read books has no advantage over the one who does.\" - Mark Twain";
+    cout << quote << endl;
+    return 0;
+}
